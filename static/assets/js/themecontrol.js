984da467eb32c7e2ed6b364bfc868bb679a32736
g10shad0w <136858133+g10shad0w@users.noreply.github.com>
2023-09-10 19:01:47 -0400

    Update themecontrol.js

--- static/assets/js/themecontrol.js
+++ static/assets/js/themecontrol.js
@@ -1,13 +1,9 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-  if (localStorage.getItem("Classic") === "on") {
-    document.body.style.backgroundColor = "#111";
-    document.body.style.color = "#ffffff";
-    document.body.style.setProperty("--bg-c", "#111");
+  if (localStorage.getItem("Classic (Dark)") === "on") {
+    document.body.style.backgroundImage = "url('circles.avif')";
     document.getElementById("Themes").value = "Classic";
   } else if (localStorage.getItem("Ocean") === "on") {
-    document.body.style.backgroundColor = "#162545";
-    document.body.style.color = "#ffffff";
-    document.body.style.setProperty("--bg-c", "#162545");
+    document.body.style.backgroundImage = "url('ocean.gif')";
     document.getElementById("Themes").value = "Ocean";
   } else if (localStorage.getItem("Midnight") === "on") {
     document.body.style.backgroundColor = "#000";
@@ -15,9 +11,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
     document.body.style.setProperty("--bg-c", "#000");
     document.getElementById("Themes").value = "Midnight";
   } else if (localStorage.getItem("Light") === "on") {
-    document.body.style.backgroundColor = "#fff";
-    document.body.style.color = "#000";
-    document.body.style.setProperty("--bg-c", "#fff");
+    document.body.style.backgroundImage = "url('white.jpeg')";
     document.getElementById("Themes").value = "Light";
   }
 });
